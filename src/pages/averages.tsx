022dea40b8dae2804db2ca595b758157784777b3
dmitry.r <acopalypse@yandex.ru>
2023-02-06 10:02:55 +0400

    front: add form structure

--- src/pages/averages.tsx
+++ src/pages/averages.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import Form from '@/components/blocks/Form/Form';
 
 export default function Averages() {
-  return <div>averages</div>;
+  const onSubmit = async (e: React.SyntheticEvent) => {};
+
+  return (
+    <>
+      <section style={{ width: '50%' }}>AveragesList</section>
+      <section style={{ width: '50%' }}>
+        <Form onSubmit={onSubmit}>
+          <input
+            type='number'
+            name='number'
+            placeholder='number'
+            step='any'
+            required
+          />
+          <div>
+            <label htmlFor='checkbox_1'>
+              <input type='checkbox' name='isNegative' id='checkbox_1' /> isNegative
+            </label>
+            <label htmlFor='checkbox_2'>
+              <input type='checkbox' name='isFractional' id='checkbox_2' /> is
+              Fractional
+            </label>
+          </div>
+          <button type={'submit'}>отправить и получить среднее</button>
+        </Form>
+      </section>
+    </>
+  );
 }
--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
+import Form from '@/components/blocks/Form/Form';
 
 export default function Messages() {
-  return <div>messages</div>;
+  const onSubmit = async (e: React.SyntheticEvent) => {};
+
+  return (
+    <>
+      <section style={{ width: '50%' }}>MessagesList</section>
+      <section style={{ width: '50%' }}>
+        <Form onSubmit={onSubmit}>
+          <input type={'text'} name='author' placeholder='author' required />
+          <input type={'text'} name='text' placeholder='message' required />
+          <button type={'submit'}>разместить сообщение</button>
+        </Form>
+      </section>
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<{}> = async () => {
